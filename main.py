e1dde6e2e79c9a3ecf3cc92af8f35496504eaf88
jasman <jasman0603@gmail.com>
2026-03-16 14:27:58 +0700

    RAG sistem FastAPi

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
 import uvicorn
+from datetime import datetime
+import tempfile
+import os
+from uuid import uuid4
+import logging
 
 from graph_model import build_graph
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage, RemoveMessage
+
+
+REQUEST_TIMEOUT_SECONDS = 60
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+
+logging.basicConfig(
+    filename=os.path.join(LOG_DIR, f"error_{datetime.now().strftime('%Y-%m-%d')}.txt"),
+    level=logging.ERROR,
+    format="%(asctime)s | %(levelname)s | %(message)s",
+)
 
 
 app = FastAPI()
@@ -13,9 +30,28 @@ async def ask_question(question: str):
         SystemMessage(content="You are a helpful assistant that answers questions based on provided documents."),
         HumanMessage(content=question)
     ]
-    response = graph.invoke({"messages": messages})
+
+    try :
+        response = graph.invoke({"messages": messages})
+
+    except TimeoutError as e:
+                  
+        logging.error(
+            f"TimeoutError: {str(e)}",
+            exc_info=True
+        )
+        return {"message": "⏱️ Request timed out. The model took too long to respond. Please try again."}   
+
+    except Exception as e:
+        error_text = str(e)
+        logging.error(
+            f"Error message :  {error_text}",
+            exc_info=True
+        )
+        return {"message": " ❌ An error occurred while processing your request. Please try again later."}  
     
     return {
+        "question": question,
         "answer": response["messages"][-1].content,
         "retrieved_documents": response["documents"],
         "similarity_scores": response["similarity_scores"],
